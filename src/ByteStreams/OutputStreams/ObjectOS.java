688923ad8f459b23b86ca42b78d67da4d2a8380c
Karthik <karthi.rs.4631@gmail.com>
2025-07-01 20:10:54 +0530

    01/07/2025

--- src/ByteStreams/OutputStreams/ObjectOS.java
+++ src/ByteStreams/OutputStreams/ObjectOS.java
@@ -1,9 +1,6 @@
 package ByteStreams.OutputStreams;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 
 class Dog implements Serializable {
     String name;
@@ -20,11 +17,19 @@ public class ObjectOS {
     public static void main(String[] args) {
         try{
             Dog dog = new Dog();
-            FileOutputStream fos = new FileOutputStream("src/files/serializtion1.txt");
+            FileOutputStream fos = new FileOutputStream("src/files/serialization1.txt");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeObject(dog);
+
+            FileInputStream fis = new FileInputStream("src/files/serialization1.txt");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Dog dog1 = (Dog)ois.readObject();
+            System.out.println(dog1.name + " " + dog1.breed + " " + dog1.color);
+
         }catch(IOException e){
             System.out.println(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
